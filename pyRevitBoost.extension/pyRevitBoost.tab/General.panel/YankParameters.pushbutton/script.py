51dbe1fb94f6b0adfa0e8b9795584ffbfa3f8e92
Zachary Mathews <zachary.c.mathews@gmail.com>
2020-06-17 10:44:04 -0400

    Add support for linked revit models

--- pyRevitBoost.extension/pyRevitBoost.tab/General.panel/YankParameters.pushbutton/script.py
+++ pyRevitBoost.extension/pyRevitBoost.tab/General.panel/YankParameters.pushbutton/script.py
@@ -2,14 +2,13 @@
 import os
 import sys
 import re
-from functools import partial, reduce
+from functools import partial
 
-from Autodesk.Revit.DB import (BuiltInParameter, FamilyInstance,
-                               FilteredElementCollector)
+from Autodesk.Revit.DB import BuiltInParameter, FamilyInstance
 
 import rpw
 from pyrevit import script, forms
-from boostutils import find_closest, get_parameter, load_as_python
+from boostutils import get_parameter, load_as_python
 
 __doc__ = '''\
 Yank parameters from nearest in linked models.
@@ -20,39 +19,18 @@ __title__ = 'Yank\nParameters'
 __author__ = 'Zachary Mathews'
 __context__ = 'Selection'
 
-uidoc = rpw.revit.uidoc
-doc = rpw.revit.doc
-view = uidoc.ActiveView
-current_phase = view.get_Parameter(BuiltInParameter.VIEW_PHASE).AsElementId()
-selection = rpw.ui.Selection()
 
-script_config = script.get_config(section='pyRevitBoost.General.YankParameters')
-reuse_config = False
-if hasattr(script_config, 'config_file'):
-    config_file=script_config.config_file
-    if os.path.isfile(script_config.config_file):
-        reuse_config = forms.alert(
-            title='YankParameters',
-            msg='Reuse previous configuration?',
-            sub_msg=config_file,
-            ok=False,
-            yes=True,
-            no=True,
-            warn_icon=False
-        )
-if not reuse_config:
-    with forms.WarningBar(title='Please select a configuration file'):
-        config_file = forms.pick_file(
-            file_ext='yaml',
-            restore_dir=True
-        )
-if not config_file:
-    sys.exit()
+def get_link_instance(title):
+    return rpw.db.Collector(
+        of_category='OST_RvtLinks',
+        of_class='RevitLinkInstance',
+        where=partial(doc_title_filter, title=title)
+    ).get_first(wrapped=False)
 
-mappings = load_as_python(config_file)
-if mappings:
-    script_config.config_file = config_file
-    script.save_config()
+
+def doc_title_filter(link, title):
+    if title in link.GetLinkDocument().Title:
+        return True
 
 
 def family_filter(el, family):
@@ -64,16 +42,35 @@ def family_filter(el, family):
     )
 
 
-def phase_filter(el, phase):
-    return get_parameter(el, builtin='PHASE_CREATED').AsValueString() == phase
+def phase_filter(el, phase, phase_map, current_phase):
+    if phase_map:
+        if phase == '<current>':
+            return get_parameter(
+                el,
+                builtin='PHASE_CREATED'
+            ).AsElementId() == phase_map[current_phase]
+        else:
+            return get_parameter(
+                el,
+                builtin='PHASE_CREATED'
+            ).AsValueString() == phase
+
+    else:
+        if phase == '<current>':
+            return get_parameter(
+                el,
+                builtin='PHASE_CREATED'
+            ).AsElementId() == current_phase
+        else:
+            return get_parameter(
+                el,
+                builtin='PHASE_CREATED'
+            ).AsValueString() == phase
 
 
 def type_filter(el, type):
     return (
-        get_parameter(
-            el,
-            builtin='ELEM_TYPE_PARAM'
-        ).AsValueString() == type
+        get_parameter(el, builtin='ELEM_TYPE_PARAM').AsValueString() == type
     )
 
 
@@ -111,7 +108,7 @@ def parameter_filter(el, parameter):
         return value <= float(filter['value'])
 
 
-def get_filters(mapping):
+def get_filters(mapping, current_phase, link_type):
     global family_filter, type_filter, phase_filter, parameter_filter
     filters = []
 
@@ -132,7 +129,9 @@ def get_filters(mapping):
     if mapping.get('phase'):
         phase_filter = partial(
             phase_filter,
-            phase=mapping['phase']
+            phase=mapping['phase'],
+            phase_map=link_type.GetPhaseMap() if link_type else None,
+            current_phase=current_phase
         )
         filters.append(phase_filter)
 
@@ -147,14 +146,21 @@ def get_filters(mapping):
     return filters
 
 
-def pair(to, others):
-    closest = find_closest(
-        to=to,
-        elements=others
-    )
+def pair(el, others, others_transform):
+    closest, min_distance = None, 0
+    for other in others:
+        if others_transform:
+            distance = others_transform.OfPoint(other.Location.Point)\
+                                       .DistanceTo(el.Location.Point)
+        else:
+            distance = other.Location.Point.DistanceTo(el.Location.Point)
+
+        if closest is None or distance < min_distance:
+            closest, min_distance = other, distance
+
     return {
         'from': closest,
-        'to': to
+        'to': el
     }
 
 
@@ -175,75 +181,146 @@ def yank(pair, mapping):
         parameter.Set(value)
 
 
-cnt = 0
-total = len(selection)
-failed = []
-not_yanked = [e.unwrap().Id for e in selection.get_elements()]
-with forms.ProgressBar(title='{value} of {max_value}') as pb:
-    with rpw.db.Transaction('Yank parameters'):
-        for mapping in mappings:
-            # Gather 'from' elements
-            filters = get_filters(mapping['from'])
-            from_elements = rpw.db.Collector(
-                of_category=mapping['from']['category'],
-                of_class=FamilyInstance,
-                view=view,
-                where=lambda e: all(f(e.unwrap()) for f in filters) \
-                                if filters else True
-            ).get_elements(wrapped=False)
+if __name__ == '__main__':
+    uidoc = rpw.revit.uidoc
+    doc = rpw.revit.doc
+    view = uidoc.ActiveView
+    current_phase = \
+        view.get_Parameter(BuiltInParameter.VIEW_PHASE).AsElementId()
+    selection = rpw.ui.Selection()
 
-            # Gather 'to' elements
-            filters = get_filters(mapping['to'])
-            to_elements = rpw.db.Collector(
-                elements=selection.get_elements(),
-                of_category=mapping['to']['category'],
-                of_class=FamilyInstance,
-                where=lambda e: all(f(e.unwrap()) for f in filters) \
-                                if filters else True
-            ).get_elements(wrapped=False)
+    script_config = script.get_config(
+        section='pyRevitBoost.General.YankParameters'
+    )
+    reuse_config = False
+    if hasattr(script_config, 'config_file'):
+        config_file=script_config.config_file
+        if os.path.isfile(script_config.config_file):
+            reuse_config = forms.alert(
+                title='YankParameters',
+                msg='Reuse previous configuration?',
+                sub_msg=config_file,
+                ok=False,
+                yes=True,
+                no=True,
+                warn_icon=False
+            )
+    if not reuse_config:
+        with forms.WarningBar(title='Please select a configuration file'):
+            config_file = forms.pick_file(
+                file_ext='yaml',
+                restore_dir=True
+            )
+    if not config_file:
+        sys.exit()
 
-            # Remove from not_yanked
-            [not_yanked.remove(e.Id) for e in to_elements if e.Id in not_yanked]
+    mappings = load_as_python(config_file)
+    if mappings:
+        script_config.config_file = config_file
+        script.save_config()
 
-            if not from_elements or not to_elements:
-                failed.extend(to_elements)
+    cnt = 0
+    total = len(selection)
+    failed = set()
+    not_yanked = set(e.unwrap().Id for e in selection.get_elements())
+    with forms.ProgressBar(title='{value} of {max_value}') as pb:
+        with rpw.db.Transaction('Yank parameters'):
+            for mapping in mappings:
+                link_instance = get_link_instance(mapping['from'].get('model'))
+                if link_instance:
+                    link_type = doc.GetElement(link_instance.GetTypeId())
+                    from_doc = link_instance.GetLinkDocument()
+                else:
+                    link_type = None
+                    from_doc = doc
+
+                # Gather 'from' elements
+                filters = get_filters(
+                    mapping['from'],
+                    current_phase=current_phase,
+                    link_type=link_type
+                )
+                from_elements = rpw.db.Collector(
+                    doc=from_doc,
+                    of_category=mapping['from']['category'],
+                    of_class=FamilyInstance,
+                    where=lambda e: all(f(e.unwrap()) for f in filters) \
+                                    if filters else True
+                ).get_elements(wrapped=False)
+
+                # Gather 'to' elements
+                filters = get_filters(
+                    mapping['to'],
+                    current_phase=current_phase,
+                    link_type=link_type
+                )
+                to_elements = rpw.db.Collector(
+                    elements=selection.get_elements(),
+                    of_category=mapping['to']['category'],
+                    of_class=FamilyInstance,
+                    where=lambda e: all(f(e.unwrap()) for f in filters) \
+                                    if filters else True
+                ).get_elements(wrapped=False)
+
+                # Remove from not_yanked
+                for e in to_elements:
+                    not_yanked.discard(e.Id)
+
+                if not from_elements or not to_elements:
+                    failed.update(to_elements)
+                    cnt += len(to_elements)
+                    pb.update_progress(
+                        new_value=cnt,
+                        max_value=total
+                    )
+                    continue
+
+                xfm = link_instance.GetTotalTransform() if link_instance else None
+                pairs = (
+                    pair(
+                        el=e,
+                        others=from_elements,
+                        others_transform=xfm
+                    ) for e in to_elements
+                )
+                for _pair in pairs:
+                    try:
+                        yank(_pair, mapping)
+                    except:
+                        failed.update(to_elements)
+                        cnt += len(to_elements)
+                        pb.update_progress(
+                            new_value=cnt,
+                            max_value=total
+                        )
+                        continue
+
                 cnt += len(to_elements)
                 pb.update_progress(
                     new_value=cnt,
                     max_value=total
                 )
-                continue
 
-            # Pair 'from' and 'to' elements based on proximity
-            pairs = [pair(e, from_elements) for e in to_elements]
-            [yank(pair, mapping) for pair in pairs]
+    if failed or not_yanked:
+        failed_msg = (
+            'Failed to yank parameters from nearest for {} selected element{}.'
+        ).format(len(failed), 's' if len(failed) > 1 else '')
 
-            cnt += len(to_elements)
-            pb.update_progress(
-                new_value=cnt,
-                max_value=total
-            )
+        not_yanked_msg = (
+            'No viable yank found for {} selected element{}.'
+        ).format(len(not_yanked), 's' if len(failed) > 1 else '')
 
-if failed or not_yanked:
-    failed_msg = (
-        'Failed to yank parameters from nearest for {} selected element{}.'
-    ).format(len(failed), 's' if len(failed) > 1 else '')
+        total_msg = (
+            'Total selected: {}'.format(len(failed) + len(not_yanked))
+        )
 
-    not_yanked_msg = (
-        'No viable yank found for {} selected element{}.'
-    ).format(len(not_yanked), 's' if len(failed) > 1 else '')
-
-    total_msg = (
-        'Total selected: {}'.format(len(failed) + len(not_yanked))
-    )
-
-    msg = (failed_msg if failed else '') + '\n\n' + \
-          (not_yanked_msg if not_yanked else '') + '\n\n' + \
-          (total_msg if failed or not_yanked else '')
-    forms.alert(
-        title='Error',
-        msg=msg
-    )
-    selection.clear()
-    selection.add(failed)
-    selection.add(not_yanked)
+        msg = (failed_msg if failed else '') + '\n\n' + \
+              (not_yanked_msg if not_yanked else '') + '\n\n' + \
+              (total_msg if failed or not_yanked else '')
+        forms.alert(
+            title='Error',
+            msg=msg
+        )
+        selection.clear()
+        selection.add(failed)
+        selection.add(not_yanked)
