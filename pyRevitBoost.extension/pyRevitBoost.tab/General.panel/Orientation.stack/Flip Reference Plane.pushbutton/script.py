f99a047e58b81d6e629e759f34e8835d0e576544
Zachary Mathews <zachary.c.mathews@gmail.com>
2020-06-23 14:47:42 -0400

    Keep hosted elements in place when flipping ref planes

--- pyRevitBoost.extension/pyRevitBoost.tab/General.panel/Orientation.stack/Flip Reference Plane.pushbutton/script.py
+++ pyRevitBoost.extension/pyRevitBoost.tab/General.panel/Orientation.stack/Flip Reference Plane.pushbutton/script.py
@@ -1,4 +1,12 @@
-from Autodesk.Revit.DB import ReferencePlane
+import clr
+clr.AddReference('System.Collections')
+from System.Collections.Generic import List
+
+from Autodesk.Revit.DB import (ElementId, ElementTransformUtils, Plane,
+                               ReferencePlane, TransactionGroup,
+                               TransactionStatus)
+from Autodesk.Revit.Exceptions import (ArgumentException,
+                                       InvalidOperationException)
 
 import rpw
 from pyrevit import forms
@@ -7,7 +15,16 @@ __doc__ = 'Flip selected reference planes.'
 __author__ = 'Zachary Mathews'
 __context__ = 'Selection'
 
+
+def get_hosted_elements(host):
+    return rpw.db.Collector(
+        of_class='FamilyInstance',
+        where=lambda e: e.Host and e.Host.Id == host.Id
+    ).get_elements(wrapped=False)
+
+
 if __name__ == '__main__':
+    doc = rpw.revit.doc
     selection = rpw.ui.Selection()
     ref_planes = (
         e for e in selection.get_elements(wrapped=False)
@@ -16,11 +33,42 @@ if __name__ == '__main__':
 
 
     failed = []
-    with rpw.db.Transaction('Flip reference planes'):
-        for rp in ref_planes:
+    for rp in ref_planes:
+        tg = TransactionGroup(doc, 'Flip reference plane')
+        if TransactionStatus.Started != tg.Start():
+            failed.append(rp.Id)
+            break
+
+        hosted_elements = []
+        with rpw.db.Transaction('Flip reference plane'):
             try:
-                rp.Flip()
-            except:
+                hosted_elements.extend(
+                    [(e.Id, e.Location.Point) for e in get_hosted_elements(rp)]
+                )
+            except AttributeError:
+                tg.RollBack()
+                failed.append(rp.Id)
+                continue
+
+            rp.Flip()
+
+        with rpw.db.Transaction('Mirror elements back to original positions'):
+            for el_id, original_pos in hosted_elements:
+                el = doc.GetElement(el_id)
+                current_pos = el.Location.Point
+                try:
+                    ElementTransformUtils.MoveElement(
+                        doc,
+                        el_id,
+                        original_pos - current_pos
+                    )
+                except InvalidOperationException:
+                    tg.RollBack()
+                    failed.append(rp.Id)
+                    break
+
+        if not tg.HasEnded():
+            if TransactionStatus.Committed != tg.Assimilate():
                 failed.append(rp.Id)
 
     if failed:
