85c882aa484784ec73c844841ece9ae3f3834fa2
zachcmathews <zachary.c.mathews@gmail.com>
2022-11-15 11:38:19 -0500

    Fix clockwise rotation

--- pyRevitBoost.extension/pyRevitBoost.tab/General.panel/Orientation2.stack/Rotate Multiple.pushbutton/script.py
+++ pyRevitBoost.extension/pyRevitBoost.tab/General.panel/Orientation2.stack/Rotate Multiple.pushbutton/script.py
@@ -1,9 +1,16 @@
 import sys
 
-from Autodesk.Revit.DB import ElementTransformUtils, Line, XYZ
-
-from pyrevit import forms
 import rpw
+from Autodesk.Revit.DB import (
+    ElementTransformUtils,
+    Line,
+    XYZ
+)
+from Autodesk.Revit.Exceptions import (
+    InvalidOperationException,
+    OperationCanceledException
+)
+from pyrevit import forms
 
 __doc__ = "Rotate multiple elements about their origins."
 __author__ = "Zachary Mathews"
@@ -12,27 +19,40 @@ __context__ = "Selection"
 uidoc = rpw.revit.uidoc
 doc = rpw.revit.doc
 
-# Get user input
-elements = [doc.GetElement(id) for id in uidoc.Selection.GetElementIds()]
-if elements:
-	try:
-		center = uidoc.Selection.PickPoint("Pick center of rotation")
-		start = uidoc.Selection.PickPoint("Pick starting point")
-		end = uidoc.Selection.PickPoint("Pick end point")
-	except:
-		sys.exit()
 
-angle = (start-center).AngleTo(end-center)
-with rpw.db.Transaction("Rotate multiple elements", doc=doc):
-	cnt = 0
-	max = len(elements)
-	with forms.ProgressBar(title='{value} of {max_value}') as pb:
-		for element in elements:
-			origin = element.Location.Point
-			a = XYZ(origin.X, origin.Y, 0)
-			b = XYZ(origin.X, origin.Y, 10)
-			axis = Line.CreateBound(a,b)
+def main():
+    elements = [doc.GetElement(_id) for _id in uidoc.Selection.GetElementIds()]
+    if elements:
+        try:
+            center = uidoc.Selection.PickPoint("Pick center of rotation")
+            start = uidoc.Selection.PickPoint("Pick starting point")
+            end = uidoc.Selection.PickPoint("Pick end point")
+        except (InvalidOperationException, OperationCanceledException):
+            sys.exit()
 
-			ElementTransformUtils.RotateElement(doc, element.Id, axis, angle)
-			cnt += 1
-			pb.update_progress(cnt, max)
+    v0 = (start - center).Normalize()
+    v1 = (end - center).Normalize()
+    angle = v0.AngleTo(v1)
+    angle = angle * -1 if v0.CrossProduct(v1).Z < 0 else angle
+    with rpw.db.Transaction("Rotate multiple elements", doc=doc):
+        cnt = 0
+        num_elements = len(elements)
+        with forms.ProgressBar(title='{value} of {max_value}') as pb:
+            for element in elements:
+                origin = element.Location.Point
+                a = XYZ(origin.X, origin.Y, 0)
+                b = XYZ(origin.X, origin.Y, 10)
+                axis = Line.CreateBound(a, b)
+
+                ElementTransformUtils.RotateElement(
+                    doc,
+                    element.Id,
+                    axis,
+                    angle
+                )
+                cnt += 1
+                pb.update_progress(cnt, num_elements)
+
+
+if __name__ == '__main__':
+    main()
